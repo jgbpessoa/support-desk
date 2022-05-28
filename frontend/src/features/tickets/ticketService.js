e35db8697cfc1670bc730056a5ef11d390ad29c9
jgbpessoa <jgbpessoa@gmail.com>
2022-05-27 21:26:53 -0300

    Fetch tickets from Backend to Redux

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "api/tickets";
+const API_URL = "api/tickets/";
 
 // Create new ticket
 const createTicket = async (ticketData, token) => {
@@ -15,8 +15,24 @@ const createTicket = async (ticketData, token) => {
   return response.data;
 };
 
+// Get user tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  console.log(response.data);
+
+  return response.data;
+};
+
 const ticketService = {
   createTicket,
+  getTickets,
 };
 
 export default ticketService;
