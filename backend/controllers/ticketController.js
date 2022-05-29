334374316fee6531cbf4fe82965c9e3e8d55a861
jgbpessoa <jgbpessoa@gmail.com>
2022-05-29 12:42:03 -0300

    Add Notes (Backend)

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -2,7 +2,6 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
 const Ticket = require("../models/ticketModel");
-const ticketModel = require("../models/ticketModel");
 
 // @desc    Get user tickets
 // @route   GET /api/tickets/
